804339d8eb3e3243b9f6d01e6c73f58727b76aee
Matias Venegas <mathystark17@gmail.com>
2024-07-10 13:44:45 -0400

    Tercer commit

--- libreria/admin.py
+++ libreria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import NavItem, Autor, Categoria, Libro
 
 # Register your models here.
+
+admin.site.register(NavItem)
+admin.site.register(Autor)
+admin.site.register(Categoria)
+admin.site.register(Libro)
+
